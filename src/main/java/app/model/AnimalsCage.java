f2abfab49168bedce06ee4535b20f9f6f86838cf
k.family <ppodpiski@yahoo.com>
2022-09-12 15:25:43 +0300

    Complete!

--- src/main/java/app/model/AnimalsCage.java
+++ src/main/java/app/model/AnimalsCage.java
@@ -3,7 +3,6 @@ package app.model;
 import lombok.Getter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
 
 
